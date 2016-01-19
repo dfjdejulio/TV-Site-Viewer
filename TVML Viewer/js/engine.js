f2a1c7007a4101aba2ebc4a7a89c08410582f002
Doug DeJulio <ddj@aisb.org>
2016-01-19 09:29:25 -0500

    Working in a rudimentary manner, but we still need to clean up events that don't apply anymore.

--- TVML Viewer/js/engine.js
+++ TVML Viewer/js/engine.js
@@ -1,6 +1,11 @@
 // Thanks to rayh on github for the sample code,
 // found here: https://gist.github.com/rayh/d023aeba28a25ed7f7f2
 
+var nextUrl;
+var lastUrl;
+var lastDoc;
+var lastDelay;
+
 var SimplePage = function(relUrl, base) {
     var self = this;
     var url = tvjsutil.urlRelativeTo(relUrl, base);
@@ -24,6 +29,11 @@ var SimplePage = function(relUrl, base) {
                     var address = addressbar.getFeature('Keyboard').text;
                     new SimplePage(address, url).load();
                     break;
+                case ('replace'):
+                    if (href) {
+                        new SimplePage(href, url).replace();
+                    }
+                    break;
                 case ('video'): // Play the "href".
                     var address = tvjsutil.urlRelativeTo(ele.getAttribute('href'), url);
                     var video = new MediaItem('video', address);
@@ -45,22 +55,45 @@ var SimplePage = function(relUrl, base) {
             }
         }
     }
+
+    self.handleDocumentDelay = function() {
+        new SimplePage(nextUrl, lastUrl).replace()
+    }
     
-    function pushDoc(document) {
+    function parseDoc(document) {
         var parser = new DOMParser();
         var doc = parser.parseFromString(document, "application/xml");
+
+        // Look for stuff to evaluate "on load" here.
+        var delay = doc.documentElement.getAttribute('delay')
+        if (delay) {
+            nextUrl = doc.documentElement.getAttribute('href')
+            lastUrl = url;
+            lastDelay = setTimeout(self.handleDocumentDelay, delay * 1000)
+            console.log('Document has delay attribute.')
+            tvjsutil.nslog('Document element has delay attribute.')
+        }
 
         doc.addEventListener("select", onSelect.bind(self));
         // Other interesting events: "play", "highlight", "holdselect",
         // or for text fields, "change".
 
-        currentdoc = doc;
+        self.currentdoc = doc;
         
-        navigationDocument.pushDocument(doc);
+        //navigationDocument.pushDocument(doc);
+        return doc;
+    }
+
+    // Not sure how well this will work.  Let's find out!
+    self.replace = function() {
+        var newDoc = parseDoc(tvjsutil.load(url));
+        navigationDocument.replaceDocument(newDoc, lastDoc);
+        lastDoc = newDoc;
     }
    
     self.load = function() {
-        pushDoc(tvjsutil.load(url))
+        lastDoc = parseDoc(tvjsutil.load(url));
+        navigationDocument.pushDocument(lastDoc);
     }
     
     return self;
